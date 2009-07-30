49729ed4ff06f302ab473d94af0e885e023a7001
h3rald <h3rald@h3rald.com>
2009-07-30 16:34:51 +0200

    Added jgfeed plugin (delicious/backtype integration). Closes #8

---
+++ ui-dev/js/feeds.js
@@ -0,0 +1,24 @@
+function feed_entry(entry){
+	var published_at = $("<span>["+$.timeago(entry.publishedDate)+"] </span>").addClass('published_at'); 
+	var link = $("<a>"+entry.title+"</a>").attr('href', entry.link);
+	return $("<li></li>").html(published_at.append(link));
+};
+function display_feed(feed, element){
+	if(!feed){
+		return false;
+	}
+	var feed_list = $("<ul></ul>")
+	for(var i=0; i<feed.entries.length; i++){
+		var entry = feed.entries[i];
+		feed_entry(entry).appendTo(feed_list);
+	}
+	feed_list.appendTo(element)
+};
+var delicious_feed = function(feed){
+	display_feed(feed, "#delicious")
+};
+var backtype_feed = function(feed){
+	display_feed(feed, "#backtype")
+};
+$.jGFeed('http://feeds.delicious.com/v2/rss/h3rald', delicious_feed, 10)
+$.jGFeed('http://feeds.backtype.com/home/21105', backtype_feed, 10)
