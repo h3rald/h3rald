080b98be892949db69b3c7bf41988f47341f6f96
h3rald <h3rald@h3rald.com>
2009-08-06 12:54:33 +0200

    XHTML 1.1 + CSS 2.1 validation

--- files/js/init.js
+++ files/js/init.js
@@ -12,7 +12,9 @@ $(document).ready(function() {
 			}
 			);
 		};
+		$('.half-hidden').css('opacity', 0.3);
 		$('.half-hidden').each(change_opacity)
+		$('.gsc-clear-button').css('opacity', 0.3);
 		$('.gsc-clear-button').each(change_opacity)
 		});
 
