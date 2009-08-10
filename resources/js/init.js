44eca83eb6ee4dba0efe67102f5b3ca735cacd13
h3rald <h3rald@h3rald.com>
2009-08-10 13:37:27 +0200

    Updated layout. * Added fleur-de-lis icons * image preloading * Fixed IE-related problems

--- resources/js/init.js
+++ resources/js/init.js
@@ -1,9 +1,9 @@
 
 $(document).ready(function() {
+		$.preloadCssImages();
 		$.slider();
 		$('#up-arrow').click(function(){ $.scrollTo('#header', 1000)});
 		$('#down-arrow').click(function(){ $.scrollTo('#footer', 1000)});
-	
 		// Drop Caps
 		$('h2 + p').each(function(){
 		var first_paragraph = this;//$('#content p')[0];
@@ -14,12 +14,11 @@ $(document).ready(function() {
 		}
 		var text = node.nodeValue;
 		var first_letter = text.substr(0,1);
-		if (first_letter.match(/[a-zA-Z]/))
-		{
+		if (first_letter.match(/[a-zA-Z]/)){
 			node.nodeValue = text.slice(1,text.length);
 			$('<span></span>').addClass('dropcap').html(first_letter).prependTo( first_paragraph );
-			});
 			Cufon.replace('.dropcap', {fontFamily: 'Mutlu'});
-		}
+			};
+		});
 });
 
