a443e54b04fd90c89f1aa1411893cdc22a250974
h3rald <h3rald@h3rald.com>
2009-08-16 14:10:34 +0200

    Removed palette, slider and icons, for now.

--- resources/js/init.js
+++ resources/js/init.js
@@ -1,13 +1,8 @@
-
 $(document).ready(function() {
-		$.preloadCssImages();
-		$.slider();
-		$('#up-arrow').click(function(){ $.scrollTo('#header', 1000)});
-		$('#down-arrow').click(function(){ $.scrollTo('#footer', 1000)});
 		// Feeds
-		backtype_comments(8);
-		$.jGFeed('http://twitter.com/statuses/user_timeline/h3rald.rss', twitter_feed, 8);
-		$.jGFeed('http://feeds.delicious.com/v2/rss/h3rald', delicious_feed, 8);
+		backtype_comments(7);
+		$.jGFeed('http://twitter.com/statuses/user_timeline/h3rald.rss', twitter_feed, 7);
+		$.jGFeed('http://feeds.delicious.com/v2/rss/h3rald', delicious_feed, 7);
 		// Drop Caps
 		$('h2 + p').each(function(){
 		var first_paragraph = this;//$('#content p')[0];
@@ -18,11 +13,9 @@ $(document).ready(function() {
 		}
 		var text = node.nodeValue;
 		var first_letter = text.substr(0,1);
-		if (first_letter.match(/[a-zA-Z]/)){
-			node.nodeValue = text.slice(1,text.length);
-			$('<span></span>').addClass('dropcap').html(first_letter).prependTo( first_paragraph );
-			Cufon.replace('.dropcap', {fontFamily: 'Mutlu', fontSize: '40px'});
-			};
+		node.nodeValue = text.slice(1,text.length);
+		$('<span></span>').addClass('dropcap').html(first_letter).prependTo( first_paragraph );
 		});
+		Cufon.replace('.dropcap', {fontFamily: 'Mutlu'});
 });
 
