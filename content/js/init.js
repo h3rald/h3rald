a2e7e211cc571a84a09ce940b3f4f4fa0d8fa620
h3rald <h3rald@h3rald.com>
2010-09-12 20:43:54 +0200

    Added Twitter button; icons; fancybox.

--- content/js/init.js
+++ content/js/init.js
@@ -15,22 +15,10 @@ function delicious_counter(data) {
 	$('#delcounter').text(text);
 }
 $(document).ready(function() {
-		$('header.home > time').text(Date.today().toString("dddd, MMMM dS yyyy"));
-		$('header.home > time').attr('datetime', Date.today().toString("yyyy-MM-dd"));
-		/////////////////////////
-		$('.timeago').timeago();
-		// TOC
-		$("#auto-toc ol").tableOfContents("#body-text", {startLevel: 1, depth: 6, topLinks: "&uarr;"});
-		// Drop Caps
-		/*
-		var first_paragraph = $('#content-body p:first');
-		if (!first_paragraph) return false;
-		var t = first_paragraph.html();
-		var first_letter = t.substr(0,1);
-		if (first_letter.match(/[a-z]/i)){
-		first_paragraph.html(t.slice(1,t.length));
-			$('<span></span>').addClass('dropcap').html(first_letter).prependTo( first_paragraph );
-		}
-		*/
-	});
+	$('header.home > time').text(Date.today().toString("dddd, MMMM dS yyyy"));
+	$('header.home > time').attr('datetime', Date.today().toString("yyyy-MM-dd"));
+	$('.timeago').timeago();
+	// TOC
+	$("#auto-toc ol").tableOfContents("#body-text", {startLevel: 1, depth: 6, topLinks: "&uarr;"});
+});
 
