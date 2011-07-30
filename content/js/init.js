36c7872482f905083f455a68e93573cb7cd6aecd
h3rald <h3rald@h3rald.com>
2011-07-30 22:51:32 +0200

    Updated stylesheet, font, javascript.

--- content/js/init.js
+++ content/js/init.js
@@ -5,22 +5,11 @@ $(function() {
 			frameHeight: 450
 		});
 		});
-function delicious_counter(data) {
-	var posts = data[0].total_posts;
-	if (!posts) return;
-	var text = posts+" bookmarks";
-	if (posts == 1) { text = posts+" bookmark" };
-	$('#delcounter').text(text);
-}
 $(document).ready(function() {
 	// Manage dates
-	//$('header.home > time').text(Date.today().toString("dddd, MMMM dS yyyy"));
-	//$('header.home > time').attr('datetime', Date.today().toString("yyyy-MM-dd"));
 	$('.timeago').timeago();
 	// Hyphenate contents 
 	Hyphenator.run();
-	// TOC
-	$("#auto-toc ol").tableOfContents("#body-text", {startLevel: 1, depth: 5, topLinks: "&uarr;"});
 	// IE6 Warning
 	var sevenup_options = {
   enableClosing: false,
