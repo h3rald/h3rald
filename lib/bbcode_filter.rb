4a30a33ac9439b1fb669e06899e0e1ffed43f3cc
h3rald <h3rald@h3rald.com>
2013-12-22 19:20:05 +0100

    Minor fixes.

--- lib/bbcode_filter.rb
+++ lib/bbcode_filter.rb
@@ -8,7 +8,16 @@ class BbcodeFilter < Nanoc3::Filter
 	identifier :bbcode
 
 	def run(content, args)
-		content.bbcode_to_html rescue content
+    custom_tags = {
+      'Small' => [
+        /\[small\](.*?)\[\/small\]/mi,
+        '<small>\1</small>',
+        'small',
+        '[small]...[/small]',
+        :small
+      ]
+    }
+    content.gsub("\n", "<br />").bbcode_to_html(custom_tags, false) rescue content
 	end
 
 end
