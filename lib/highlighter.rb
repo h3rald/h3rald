96a89ab19859283b4a8a03c7c0a4fec0c1d7d533
h3rald <h3rald@h3rald.com>
2013-12-22 14:17:49 +0100

    Added Lato font, highlight.js support. Closes #65.

--- lib/highlighter.rb
+++ lib/highlighter.rb
@@ -1,15 +1,17 @@
 # Monkey patch Nanoc::Helpers::Filtering
-require 'coderay'
+#require 'coderay'
 
 module Nanoc3::Helpers::Filtering
 
 	def highlight(syntax, &block)
-		data = capture(&block)
+		data = capture(&block).strip
 		# Reconvert <% %>
+    data.gsub! /</, '&lt;'
+    data.gsub! />/, '&gt;'
 		data.gsub! /&lt;%/, '<%'
 		data.gsub! /%&gt;/, '%>'
-		filtered_data = CodeRay.scan(data.strip, syntax).div(:line_numbers => :inline, :tab_width => 2, :css =>:class)
-		filtered_data = "<notextile>\n"+filtered_data+"\n</notextile>"
+		#filtered_data = CodeRay.scan(data.strip, syntax).div(:line_numbers => :inline, :tab_width => 2, :css =>:class)
+		filtered_data = "<notextile>\n<div class='#{syntax}'><pre><code>\n"+data+"\n</code></pre></div></notextile>"
 		buffer = eval('_erbout', block.binding)
 		buffer << filtered_data
 	end
