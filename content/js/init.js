b4947972fa0b0c4c7409f91606c10f8ca3e41df4
h3rald <h3rald@h3rald.com>
2014-03-08 15:11:50 +0100

    Fixed responsive navbar, added shadows.

--- content/js/init.js
+++ content/js/init.js
@@ -31,5 +31,22 @@ $(document).ready(function() {
       h3rald_search();
     }
   })
+  var navHeight = $('.navbar-collapse').height();
+
+  $('.navbar-collapse').on('show.bs.collapse', function(){
+    if($(this).height() != 0){
+      navHeight = $(this).height();
+    }
+    $('body').animate({
+      'padding-top': parseInt($("body").css("padding-top")) + navHeight
+    }, 300);
+  });
+
+  $('.navbar-collapse').on('hide.bs.collapse', function(){
+    navHeight = $(this).height();
+    $('body').animate({
+      'padding-top': parseInt($("body").css("padding-top")) - navHeight
+    }, 300);
+  });
 });
 
